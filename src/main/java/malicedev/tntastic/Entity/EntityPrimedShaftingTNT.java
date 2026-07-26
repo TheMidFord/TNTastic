03e045f493a05f0fe6fe4aa87b6d315190ba16d9
MidFord <71330862+TheMidFord@users.noreply.github.com>
2026-07-26 02:35:51 +0200

    Trying to make the Mineshaft Excavation TNT better...

--- src/main/java/malicedev/tntastic/Entity/EntityPrimedShaftingTNT.java
+++ src/main/java/malicedev/tntastic/Entity/EntityPrimedShaftingTNT.java
@@ -1,15 +1,29 @@
 package malicedev.tntastic.Entity;
 
-import net.minecraft.client.Minecraft;
+import net.minecraft.core.block.Block;
+import net.minecraft.core.block.Blocks;
 import net.minecraft.core.entity.Entity;
 import net.minecraft.core.entity.EntityPrimedTNT;
+import net.minecraft.core.enums.EnumDropCause;
+import net.minecraft.core.util.helper.MathHelper;
 import net.minecraft.core.world.World;
+import net.minecraft.core.world.pos.TilePos;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class EntityPrimedShaftingTNT extends EntityPrimedTNT {
 	public EntityPrimedShaftingTNT(World world) {
 		super(world);
+
 	}
 	public int explosions;
+	public static final Set<Block<?>> INDESTRUCTIBLES = new HashSet<>();
+	static {
+		INDESTRUCTIBLES.add(Blocks.OBSIDIAN);
+		INDESTRUCTIBLES.add(Blocks.MOBSPAWNER);
+		INDESTRUCTIBLES.add(Blocks.BEDROCK);
+	}
 
 
 	public EntityPrimedShaftingTNT(World world, double x, double y, double z) {
@@ -32,7 +46,7 @@ public class EntityPrimedShaftingTNT extends EntityPrimedTNT {
 		this.yo = this.y;
 		this.zo = this.z;
 		this.yd -= 0.04;
-		this.move(this.xd, this.yd, this.zd);
+		this.move(0, this.yd, 0);
 		this.xd *= 0.98;
 		this.yd *= 0.98;
 		this.zd *= 0.98;
@@ -44,7 +58,7 @@ public class EntityPrimedShaftingTNT extends EntityPrimedTNT {
 
 		if (this.fuse-- <= 0) {
 			if (!this.world.isClientSide) {
-				this.world.createExplosion((Entity)null, this.x, this.y + (double)0.5F, this.z, 4.0F);
+				explodeVertical();
 				this.move(0,0.3,0);
 				this.explosions++;
 				this.fuse = 2;
@@ -65,5 +79,23 @@ public class EntityPrimedShaftingTNT extends EntityPrimedTNT {
 			this.world.spawnParticle("smoke", this.x, this.y + (double)0.5F, this.z, (double)0.0F, (double)0.0F, (double)0.0F, 0, false);
 		}
 
+	}
+
+	public void explodeVertical(){
+		TilePos queryPos = new TilePos();
+
+		for (int rows = -2; rows <3; rows++){
+			for (int columns = -2; columns<3; columns++){
+				var block = world.getBlockType(queryPos.set(MathHelper.floor(this.x),MathHelper.floor(this.y),MathHelper.floor(this.z)).add(rows,-1,columns));
+				if (block != Blocks.AIR && !block.isEntityTile && !INDESTRUCTIBLES.contains(block)){
+					block.dropWithCause(world,EnumDropCause.EXPLOSION,queryPos,this.world.getBlockData(queryPos),null,null);
+					this.world.setBlockTypeNotify(queryPos, Blocks.AIR);
+					block.onDestroyedByExplosion(this.world,queryPos);
+					this.world.spawnParticle("explode", (this.z+rows), (this.y-1),(this.z+columns),1,1,1, 0, false);
+				}
+			}
+
+		}
+		this.world.spawnParticle("smoke", (this.x), (this.y),(this.z), 1,1, 1, 0, false);
 	}
 }
